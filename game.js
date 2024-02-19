5d26b9e39ac89b8fa4074f9bc2c653e3acad7f6f
Burt <154017141+Burt8@users.noreply.github.com>
2024-02-19 16:19:23 -0500

    Add new function, swap existing with new
    Changed getComputerChoice function to rng
    getComputerChoice now invokes rng to get a random number, that random number will then return 1 of 3 strings based off of the number range

--- game.js
+++ game.js
@@ -1,6 +1,18 @@
-function getComputerChoice() {
+function rng() {
         let result = Math.floor(Math.random() * 100) + 1;
         return result 
     }
-    
+
+function getComputerChoice() {
+    let result = rng()
+    console.log(result)
+    if (result <= 33) {
+        return("Rock");
+    } else if (result >= 34 && result <= 66) {
+        return("Paper")
+    } else if (result >= 67) {
+        return("Scissors")
+    }
+}
+
 console.log(getComputerChoice())
