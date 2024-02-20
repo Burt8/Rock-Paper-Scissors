88f54d53aee8d0648d401045bbe777107e988e4d
Burt <154017141+Burt8@users.noreply.github.com>
2024-02-19 21:38:02 -0500

    Add all Rock and Scissors scenarios to gamePlay
    Added all Rock scenarios to gamePlay function
    Added all Scissors scenarios to gamePlay function

--- game.js
+++ game.js
@@ -19,8 +19,20 @@ function gamePlay(playerSelection,computerSelection) {
     if (playerSelection === "Paper" && computerSelection ==="Rock") {
         return("You Win! Paper beats Rock!")
     } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        return("Tie!")
+        return("Tie! Paper Paper!")
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         return("You Lose! Scissors beat Paper!")
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return("You Lose! Paper beats Rock!")
+    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
+        return("Tie! Rock Rock!")
+    } else if (playerSelection === "Rock" && computerSelection ==="Scissors") {
+        return("You Win! Rock beats Scissors!")
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        return("Tie! Scissors Scissors!")
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return("You Lose! Rock beats Scissors!")
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return("You Win! Scissors beat Paper!")
     }
 }
